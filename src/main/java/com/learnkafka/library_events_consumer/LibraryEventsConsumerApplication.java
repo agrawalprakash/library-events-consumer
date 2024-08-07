7f47f2589c7dec5c8dddb932fe35b14bfa905ed6
agrawalprakash <33377123+agrawalprakash@users.noreply.github.com>
2024-08-07 09:19:46 +0530

    LibraryEventsConsumerConfig

--- src/main/java/com/learnkafka/library_events_consumer/LibraryEventsConsumerApplication.java
+++ src/main/java/com/learnkafka/library_events_consumer/LibraryEventsConsumerApplication.java
@@ -2,12 +2,19 @@ package com.learnkafka.library_events_consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.listener.KafkaMessageListenerContainer;
 
 @SpringBootApplication
 public class LibraryEventsConsumerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryEventsConsumerApplication.class, args);
+
+		KafkaMessageListenerContainer
+
+				ConcurrentMessageListenerContainer
+				ConcurrentMessageListenerContainer
+
 	}
 
 }
